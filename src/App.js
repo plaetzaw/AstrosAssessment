9edabe37b14aeb737f3ea536363d2664090a73ea
plaetzaw <plaetzaw@gmail.com>
2021-06-29 15:56:22 -0500

    Sorted pitches by type

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [pitches, setPitches] = useState(null)
+  const [selectedPitch, setSelectedPitch] = useState([])
+  const [twoseam, setTwoSeam] = useState(null)
+  const [fourseam, setFourSeam] = useState([])
+  const [curve, setCurve] = useState(null)
+  const [change, setChange] = useState(null)
+
+  useEffect(async () => {
+    const forseam = []
+    const twoseam = []
+    const slider = []
+    const sinker = []
+    const curve = []
+    const cutter = []
+    const changeup = []
+    const splitter = []
+
+    const res = await axios.get('https://raw.githubusercontent.com/rd-astros/hiring-resources/master/pitches.json')
+    // console.log(res.data.queryResults.row)
+    const wait = await setPitches(res.data.queryResults.row)
+    for (let i = 0; i < pitches.length; i++) {
+      // console.log(pitches[i].pitch_name)
+      if (pitches[i].pitch_name === 'Four-seam FB') {
+        forseam.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Two-seam FB') {
+        twoseam.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Slider') {
+        slider.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Sinker') {
+        sinker.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Sinker') {
+        sinker.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Curveball') {
+        curve.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Cutter') {
+        cutter.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Changeup') {
+        changeup.push(pitches[i])
+      }
+      if (pitches[i].pitch_name === 'Splitter') {
+        splitter.push(pitches[i])
+      }
+    }
+    console.log(fourseam, twoseam, slider, sinker, curve, cutter, changeup, splitter)
+  }, [selectedPitch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
